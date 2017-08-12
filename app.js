aff4b61a06144c18418e6493d109e96979a04d60
xUkroPx <xzahermazohx@gmail.com>
2017-08-12 14:48:38 +0300

    adding index.html, app.js and style.scss

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by night on 8/12/2017.
+ */
+var image = React.createElement('img', {src: 'https://facebook.github.io/react/img/logo.svg'});
+var title = React.createElement('h1', null, 'Hello, React!');
+var subtitle = React.createElement('p', null, 'Первый опыт создания контейнера в React');
+var container = React.createElement('div', {className: 'container'}, image, title, subtitle);
+
+ReactDOM.render(container, document.getElementById('start'));
